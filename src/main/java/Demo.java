5467b63fc564404158cb47645542fe106819221f
hzxton <128694554+hzxton@users.noreply.github.com>
2023-04-15 18:22:20 +0800

    Update Demo.java

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -2,5 +2,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hot1");
+        System.out.println("hot1");
     }
 }
